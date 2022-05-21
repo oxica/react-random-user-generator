945077fc7ddc033be4c8cdf6780da6c2e460df65
Oksana <kskaplun@gmail.com>
2022-05-22 00:03:07 +0300

    add user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import User from './User/User';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <User />
     </div>
   );
 };
---
+++ src/components/User/User.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+export default class FetchRandomUser extends Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results[0], loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+      <div>
+        <div>{this.state.person.name.title}</div>
+        <div>{this.state.person.name.first}</div>
+        <div>{this.state.person.name.last}</div>
+        <img src={this.state.person.picture.large} alt="user" />
+      </div>
+    );
+  }
+}
